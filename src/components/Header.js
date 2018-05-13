7031b8da97f0dd2b38b7a8f14c32b77c833f6f41
unknown <mrsanchezs@hotmail.com>
2018-05-12 20:05:42 -0500

    Session 09

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,25 +2,64 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class Header extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            searchVisible: false
+        }
+    }
+
+    //toggle visibility when run on the state
+    showSearch(){
+        this.setState({
+            searchVisible: !this.state.searchVisible
+        })
+    }
+
     render(){
+        //Classes to add to the <input /> element
+        let searchInputClasses = ["searchInput"];
+
+        //Update the class array if the state is visible
+        if(this.state.searchVisible){
+            searchInputClasses.push("active");
+        }
+
+        const wrapperStyle = {
+            backgroundColor: 'rgba(251, 202, 43, 1)'
+        }
+
+        const titleStyle = {
+            color: '#111111'
+        }
+
+        const menuColor = {
+            backgroundColor: '#111111'
+        }
+
         return (
-            <div className="header">
+            <div style={ wrapperStyle } className="header">
                 <div className="menuIcon">
-                    <div className="dashTop"></div>
-                    <div className="dashBottom"></div>
-                    <div className="circle"></div>
+                    <div className="dashTop" style={ menuColor}></div>
+                    <div className="dashBottom" style={ menuColor}></div>
+                    <div className="circle" style={ menuColor}></div>
                 </div>
 
-                <span className="title">
+                <span style={titleStyle} className="title">
                     { this.props.title }
                 </span>
 
                 <input
                     type="text"
-                    className="searchInput"
+                    className={searchInputClasses.join(' ')}
                     placeholder="Search..." />
 
-                <div className="fa fa-search searchIcon"></div>
+                {/* Adding an onClick handler to call the showSearch button */}
+                <div
+                    style = {titleStyle}
+                    onClick={this.showSearch.bind(this)} 
+                    className="fa fa-search searchIcon"></div>
             </div>
         )
     }
